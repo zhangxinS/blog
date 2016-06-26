a13d8fd42d91cba31bad8396a48b43de00cff27a
zhangxinS <S1130018964@outlook.com>
2016-06-26 11:59:41 +0800

    添加后台

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,5 +20,15 @@ Route::get('blog/{slug}','BlogController@showPost');
 
 //Admin
 Route::get('admin',function () {
-    return redirect('/admin/post');
+   return redirect('/admin/post');
 });
+Route::group(['namespace' => 'Admin','middleware' => 'auth'],function () {
+   Route::resource('admin/post','PostController');
+   Route::resource('admin/tag','TagController');
+   Route::get('admin/upload','UploadController@index');
+});
+
+//登录  登出
+Route::get('/auth/login','Auth\AuthController@getLogin');
+Route::post('/auth/login','Auth\AuthController@postLogin');
+Route::get('/auth/logout','Auth\AuthController@getLogout');
