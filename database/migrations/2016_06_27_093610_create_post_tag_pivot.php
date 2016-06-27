c9e59cf888c893f99cd088236bc65808b7b51aea
zhangxinS <1130018964@qq.com>
2016-06-27 17:37:54 +0800

    创建标签model和表迁移

---
+++ database/migrations/2016_06_27_093610_create_post_tag_pivot.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTagPivot extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag_pivot', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('post_tag_pivot');
+    }
+}
